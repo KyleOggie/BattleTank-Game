2350e491f8e983080571e3fa3b74239987baa61a
DESKTOP-RJVQBPF\ogbou <kyleeee333@gmail.com>
2018-06-26 12:17:13 +0100

    Lecture 201 - FMath Error

--- BattleTankGame/Source/BattleTankGame/Public/Tank.h
+++ BattleTankGame/Source/BattleTankGame/Public/Tank.h
@@ -17,7 +17,8 @@ class BATTLETANKGAME_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-
+	// Called by the engine when actor damage is dealt
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
 protected:
 	// Called when the game starts or when spawned
@@ -27,6 +28,10 @@ private:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int32 StartingHealth = 100;
 
+	UPROPERTY(VisibleAnywhere, Category = "Health")
+	int32 CurrentHealth = StartingHealth;
 
 };
